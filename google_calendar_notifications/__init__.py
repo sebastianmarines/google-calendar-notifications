6762e1f1cd66eb74d585479351ee7f41f62476a7
Sebastian Marines <sebastian0marines@gmail.com>
2021-09-04 13:48:16 -0500

    Filter events that start after current datetime

--- google_calendar_notifications/__init__.py
+++ google_calendar_notifications/__init__.py
@@ -1,1 +1,2 @@
 __version__ = '0.1.0'
+app_name = "Google Calendar notifications"
--- google_calendar_notifications/main.py
+++ google_calendar_notifications/main.py
@@ -1,6 +1,8 @@
+import datetime
 from pathlib import Path
 from typing import List, Optional
 
+import notify2
 from gcsa.event import Event
 from gcsa.google_calendar import GoogleCalendar
 from google.auth.transport.requests import Request
@@ -8,9 +10,11 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 
+from google_calendar_notifications import app_name
 from google_calendar_notifications.settings import CREDENTIALS, SCOPES
 
 if __name__ == "__main__":
+    notify2.init(app_name)
     home = Path.home()
     app_directory = home / ".google-calendar-notifications"
     if not app_directory.exists():
@@ -34,14 +38,19 @@ if __name__ == "__main__":
         'primary', 'ii0ct0rqg5vrnpq7v5dtrojnn4@group.calendar.google.com',
         'ejab4nspdr9q0mku2gjp860f20@group.calendar.google.com'
     ]
+    today = datetime.date.today()
+    time_min = datetime.datetime.combine(today, datetime.datetime.min.time())
+    time_max = datetime.datetime.combine(today, datetime.datetime.max.time())
 
     calendars: List[GoogleCalendar] = [
         GoogleCalendar(credentials=creds, calendar=cal_id)
         for cal_id in calendar_ids
     ]
 
-    events: List[Optional[Event]] = [
-        event for calendar in calendars for event in calendar.get_events()
+    events: List[Event] = [
+        event for calendar in calendars
+        for event in calendar.get_events(time_min=time_min, time_max=time_max)
+        if event if event.start > datetime.datetime.now(event.start.tzinfo)
     ]
 
     for event in events:

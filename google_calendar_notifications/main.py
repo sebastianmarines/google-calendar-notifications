a421dc3d92c2bb9d75368eba12967124aecd4c4d
Sebastian Marines <sebastian0marines@gmail.com>
2021-09-04 12:49:36 -0500

    Get list of events

--- google_calendar_notifications/main.py
+++ google_calendar_notifications/main.py
@@ -1,11 +1,14 @@
 from pathlib import Path
+from typing import List, Optional
 
-from typing import Optional
+from gcsa.event import Event
+from gcsa.google_calendar import GoogleCalendar
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
 
 from google_calendar_notifications.settings import CREDENTIALS, SCOPES
-from google_auth_oauthlib.flow import InstalledAppFlow
 
 if __name__ == "__main__":
     home = Path.home()
@@ -27,3 +30,24 @@ if __name__ == "__main__":
             creds = flow.run_local_server(port=0)
         with open(creds_file, 'w') as token:
             token.write(creds.to_json())
+    calendar_ids = [
+        'primary', 'ii0ct0rqg5vrnpq7v5dtrojnn4@group.calendar.google.com',
+        'ejab4nspdr9q0mku2gjp860f20@group.calendar.google.com'
+    ]
+
+    calendars: List[GoogleCalendar] = [
+        GoogleCalendar(credentials=creds, calendar=cal_id)
+        for cal_id in calendar_ids
+    ]
+
+    events: List[Optional[Event]] = [
+        event for calendar in calendars for event in calendar.get_events()
+    ]
+
+    for event in events:
+        if event:
+            print("Reminders: ", event.reminders)
+            print("Start date: ", event.start)
+            print("Description: ", event.description)
+            print("Summary: ", event.summary)
+            print("-----")
